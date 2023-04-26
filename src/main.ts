94f6014a74fdaea3b6a5cb6537f5067af3e4782d
Debasis Kar <debasiskar@Debasiss-MacBook-Air.local>
2023-04-26 21:32:27 +0530

    added partials in hbs

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,34 @@
 import { NestFactory } from '@nestjs/core';
-import { join } from 'path';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common';
 import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify';
-import session from 'express-session';
-// import secureSession from '@fastify/secure-session';
-// import cookieParser from 'cookie-parser';
-// import { IronSession } from "iron-session";
-// import fastifyCookie from '@fastify/cookie';
+import { AppModule } from './app.module';
+import { join } from 'path';
+
 async function bootstrap() {
-  // const app = await NestFactory.create<NestExpressApplication>(
-  //   AppModule,
-  // );
-
-
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-
-
-  // await app.register(secureSession, {
-  //   secret: 'averylogphrasebiggerthanthirtytwochars',
-  //   salt: 'mq9hDxBVDbspDR6n',
-  // });
-
-  // await app.register(fastifyCookie, {
-  //   secret: 'my-secret', // for cookies signature
-  // });
-
-  app.enableCors({
-    credentials: true,
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-    ],
-  });
-  // app.use(cookieParser());
-
-  app.use(
-    session({
-      secret: 'my-secret',
-      resave: false,
-      // saveUninitialized: true,
-    }),
-  );
-
-
   app.useStaticAssets({
     root: join(__dirname, '..', 'public'),
     prefix: '/public/',
+
   });
   app.setViewEngine({
     engine: {
       handlebars: require('handlebars'),
+
     },
     templates: join(__dirname, '..', 'views'),
+    options: {
+      partials: {
+        header: 'partials/header.hbs',
+        footer: 'partials/footer.hbs'
+      }
+    }
+
+
   });
-
-
-  app.useGlobalPipes(new ValidationPipe());
-  // app.useStaticAssets(join(__dirname, '..', 'public'));
-  // app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  // app.setViewEngine('hbs');
-
-
-
-  
-  // app.useStaticAssets({
-  //   root: join(__dirname, '..', 'public'),
-  //   prefix: '/public/',
-  // });
-  // app.setViewEngine({
-  //   engine: {
-  //     handlebars: require('handlebars'),
-  //   },
-  //   templates: join(__dirname, '..', 'views'),
-  // });
-
-
-
-
-
-  await app.listen(4000);
+  // app.registerPartials
+  await app.listen(3000);
 }
 bootstrap();
